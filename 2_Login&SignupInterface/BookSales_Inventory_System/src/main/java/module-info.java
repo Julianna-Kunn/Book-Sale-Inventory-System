292699b7156759ee26abad18a8d2def74002a3b7
Azanykho03 <167041067+Azanykho03@users.noreply.github.com>
2024-04-24 09:28:08 +0800

    Delete 2_Login&SignupInterface directory

--- 2_Login&SignupInterface/BookSales_Inventory_System/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.booksales_inventory_system {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens org.example.booksales_inventory_system to javafx.fxml;
-    exports org.example.booksales_inventory_system;
-}
