9ed081760634e7e28577ce9ef22ccbcdab3c5383
Azanykho03 <167041067+Azanykho03@users.noreply.github.com>
2024-05-05 04:03:14 +0800

    Delete 2_LoginInterFace&2_SignUpInterFace directory

--- 2_LoginInterFace&2_SignUpInterFace/System2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.system2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens org.example.system2 to javafx.fxml;
-    exports org.example.system2;
-}
